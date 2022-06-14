4ac357afa1eae7dc505b4e035f1acb2b5eef261f
IoriBranford <ioribranford@gmail.com>
2022-06-14 15:07:44 -0700

    Load and draw the map.

---
+++ data/stage_caravan.lua
@@ -0,0 +1,441 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.8.5",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 30,
+  height = 20,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 9,
+  nextobjectid = 1,
+  backgroundcolor = { 77, 39, 6 },
+  properties = {},
+  tilesets = {
+    {
+      name = "Beehive",
+      firstgid = 1,
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 30,
+      image = "Beehive.png",
+      imagewidth = 480,
+      imageheight = 256,
+      objectalignment = "unspecified",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 16,
+        height = 16
+      },
+      properties = {},
+      wangsets = {
+        {
+          name = "Unnamed Set",
+          tile = -1,
+          properties = {},
+          colors = {
+            {
+              color = { 255, 0, 0 },
+              name = "Floor",
+              probability = 1,
+              tile = -1,
+              properties = {}
+            },
+            {
+              color = { 0, 255, 0 },
+              name = "Honey",
+              probability = 1,
+              tile = -1,
+              properties = {}
+            },
+            {
+              color = { 0, 0, 255 },
+              name = "Light floor",
+              probability = 1,
+              tile = -1,
+              properties = {}
+            },
+            {
+              color = { 255, 119, 0 },
+              name = "Dark floor",
+              probability = 1,
+              tile = -1,
+              properties = {}
+            }
+          },
+          wangtiles = {
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 240
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 241
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 242
+            },
+            {
+              wangid = { 2, 1, 2, 1, 2, 1, 2, 1 },
+              tileid = 245
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 270
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 271
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 272
+            },
+            {
+              wangid = { 1, 1, 2, 2, 2, 1, 1, 1 },
+              tileid = 273
+            },
+            {
+              wangid = { 1, 1, 2, 2, 2, 2, 2, 1 },
+              tileid = 274
+            },
+            {
+              wangid = { 1, 1, 1, 1, 2, 2, 2, 1 },
+              tileid = 275
+            },
+            {
+              wangid = { 2, 2, 1, 1, 1, 2, 2, 2 },
+              tileid = 276
+            },
+            {
+              wangid = { 2, 2, 1, 1, 1, 1, 1, 2 },
+              tileid = 277
+            },
+            {
+              wangid = { 2, 2, 2, 2, 1, 1, 1, 2 },
+              tileid = 278
+            },
+            {
+              wangid = { 2, 2, 2, 2, 2, 2, 2, 0 },
+              tileid = 288
+            },
+            {
+              wangid = { 2, 0, 2, 2, 2, 2, 2, 2 },
+              tileid = 289
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 300
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 301
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 302
+            },
+            {
+              wangid = { 2, 2, 2, 2, 2, 1, 1, 1 },
+              tileid = 303
+            },
+            {
+              wangid = { 2, 2, 2, 2, 2, 2, 2, 2 },
+              tileid = 304
+            },
+            {
+              wangid = { 2, 1, 1, 1, 2, 2, 2, 2 },
+              tileid = 305
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 2, 2, 2 },
+              tileid = 306
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 307
+            },
+            {
+              wangid = { 1, 2, 2, 2, 1, 1, 1, 1 },
+              tileid = 308
+            },
+            {
+              wangid = { 2, 2, 2, 2, 2, 0, 2, 2 },
+              tileid = 318
+            },
+            {
+              wangid = { 2, 2, 2, 0, 2, 2, 2, 2 },
+              tileid = 319
+            },
+            {
+              wangid = { 0, 0, 2, 2, 2, 0, 0, 0 },
+              tileid = 320
+            },
+            {
+              wangid = { 0, 0, 2, 2, 2, 2, 2, 0 },
+              tileid = 321
+            },
+            {
+              wangid = { 0, 0, 0, 0, 2, 2, 2, 0 },
+              tileid = 322
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 330
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 331
+            },
+            {
+              wangid = { 1, 1, 1, 1, 1, 1, 1, 1 },
+              tileid = 332
+            },
+            {
+              wangid = { 2, 2, 2, 1, 1, 1, 1, 1 },
+              tileid = 333
+            },
+            {
+              wangid = { 2, 2, 2, 1, 1, 1, 2, 2 },
+              tileid = 334
+            },
+            {
+              wangid = { 2, 1, 1, 1, 1, 1, 2, 2 },
+              tileid = 335
+            },
+            {
+              wangid = { 1, 1, 1, 2, 2, 2, 2, 2 },
+              tileid = 336
+            },
+            {
+              wangid = { 1, 1, 1, 2, 2, 2, 1, 1 },
+              tileid = 337
+            },
+            {
+              wangid = { 1, 2, 2, 2, 2, 2, 1, 1 },
+              tileid = 338
+            },
+            {
+              wangid = { 2, 2, 2, 2, 2, 0, 0, 0 },
+              tileid = 350
+            },
+            {
+              wangid = { 2, 2, 2, 2, 2, 2, 2, 2 },
+              tileid = 351
+            },
+            {
+              wangid = { 2, 0, 0, 0, 2, 2, 2, 2 },
+              tileid = 352
+            },
+            {
+              wangid = { 3, 1, 3, 1, 3, 1, 3, 1 },
+              tileid = 362
+            },
+            {
+              wangid = { 4, 1, 4, 1, 4, 1, 4, 1 },
+              tileid = 365
+            },
+            {
+              wangid = { 2, 2, 2, 0, 0, 0, 0, 0 },
+              tileid = 380
+            },
+            {
+              wangid = { 2, 2, 2, 0, 0, 0, 2, 2 },
+              tileid = 381
+            },
+            {
+              wangid = { 2, 0, 0, 0, 0, 0, 2, 2 },
+              tileid = 382
+            },
+            {
+              wangid = { 1, 1, 3, 3, 3, 1, 1, 1 },
+              tileid = 390
+            },
+            {
+              wangid = { 1, 1, 3, 3, 3, 3, 3, 1 },
+              tileid = 391
+            },
+            {
+              wangid = { 1, 1, 1, 1, 3, 3, 3, 1 },
+              tileid = 392
+            },
+            {
+              wangid = { 1, 1, 4, 4, 4, 1, 1, 1 },
+              tileid = 393
+            },
+            {
+              wangid = { 1, 1, 4, 4, 4, 4, 4, 1 },
+              tileid = 394
+            },
+            {
+              wangid = { 1, 1, 1, 1, 4, 4, 4, 1 },
+              tileid = 395
+            },
+            {
+              wangid = { 3, 3, 3, 3, 3, 1, 1, 1 },
+              tileid = 420
+            },
+            {
+              wangid = { 3, 3, 3, 3, 3, 3, 3, 3 },
+              tileid = 421
+            },
+            {
+              wangid = { 3, 1, 1, 1, 3, 3, 3, 3 },
+              tileid = 422
+            },
+            {
+              wangid = { 4, 4, 4, 4, 4, 1, 1, 1 },
+              tileid = 423
+            },
+            {
+              wangid = { 4, 4, 4, 4, 4, 4, 4, 4 },
+              tileid = 424
+            },
+            {
+              wangid = { 4, 1, 1, 1, 4, 4, 4, 4 },
+              tileid = 425
+            },
+            {
+              wangid = { 3, 3, 3, 1, 1, 1, 1, 1 },
+              tileid = 450
+            },
+            {
+              wangid = { 3, 3, 3, 1, 1, 1, 3, 3 },
+              tileid = 451
+            },
+            {
+              wangid = { 3, 1, 1, 1, 1, 1, 3, 3 },
+              tileid = 452
+            },
+            {
+              wangid = { 4, 4, 4, 1, 1, 1, 1, 1 },
+              tileid = 453
+            },
+            {
+              wangid = { 4, 4, 4, 1, 1, 1, 4, 4 },
+              tileid = 454
+            },
+            {
+              wangid = { 4, 1, 1, 1, 1, 1, 4, 4 },
+              tileid = 455
+            }
+          }
+        }
+      },
+      tilecount = 480,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "group",
+      id = 3,
+      name = "background",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      layers = {
+        {
+          type = "tilelayer",
+          x = 0,
+          y = 0,
+          width = 30,
+          height = 20,
+          id = 8,
+          name = "underfloor",
+          visible = true,
+          opacity = 1,
+          offsetx = 0,
+          offsety = 0,
+          parallaxx = 1,
+          parallaxy = 1,
+          properties = {},
+          encoding = "base64",
+          compression = "zlib",
+          chunks = {
+            {
+              x = -16, y = 0, width = 16, height = 14,
+              data = "eJxjYBgFo2AUkAOeUKifiZGBAQBAGwDo"
+            },
+            {
+              x = 0, y = 0, width = 16, height = 14,
+              data = "eJxjYBgFo2DwgzYcmBg1j4D4MQ5MjBoGRiDCgYlRI8eI218At4IVvA=="
+            },
+            {
+              x = 16, y = 0, width = 16, height = 14,
+              data = "eJxjYBgFo2DgwCNKDWCkTLschfopBQD1vgED"
+            },
+            {
+              x = -16, y = 14, width = 16, height = 14,
+              data = "eJxjYCAfKDBSoHkUjHjwhEL9TBSmv8GQfgFqMAEq"
+            },
+            {
+              x = 0, y = 14, width = 16, height = 14,
+              data = "eJxjZGRgYARiOSAeBUMLtJGgDh0/AuLHROp/jAUzQNMNNrPR7WFkxMSw9IbNbHR78AFsZmOzZ7ACACQ4FgI="
+            },
+            {
+              x = 16, y = 14, width = 16, height = 14,
+              data = "eJxjYBgFAwnagPgRBfofgwhG8vUzAvXKUaB/FFAGAJAxAm4="
+            }
+          }
+        },
+        {
+          type = "tilelayer",
+          x = 0,
+          y = 0,
+          width = 30,
+          height = 20,
+          id = 1,
+          name = "floor",
+          visible = true,
+          opacity = 1,
+          offsetx = 0,
+          offsety = 0,
+          parallaxx = 1,
+          parallaxy = 1,
+          properties = {},
+          encoding = "base64",
+          compression = "zlib",
+          chunks = {
+            {
+              x = -16, y = 0, width = 16, height = 14,
+              data = "eJxjYBgFo2BoAlZGyvRrU8cZFAEAHkMAMg=="
+            },
+            {
+              x = 0, y = 0, width = 16, height = 14,
+              data = "eJxjYBgFIxGwMjIwsDGSphYZ8zFAMC55ZCwGVKODA8PMwqdGF4/biFEzmAEAe5kDhg=="
+            },
+            {
+              x = 16, y = 0, width = 16, height = 14,
+              data = "eJxjYBgFQxmwM1Kmn59C+3Up1D/SAQBgPwBF"
+            },
+            {
+              x = -16, y = 14, width = 16, height = 14,
+              data = "eJxjYBgFQxUoM1KmX4BC+/Uo1D8KGBgAlYcAYw=="
+            },
+            {
+              x = 0, y = 14, width = 16, height = 14,
+              data = "eJxjYBhZQJmRgUEFCROjBhkLMqBibHrQ1WDDJkCsz0BYD0wdOjaA2kvIHiEC4YHNbGz2jALaAACZXgeF"
+            },
+            {
+              x = 16, y = 14, width = 16, height = 14,
+              data = "eJxjYBjaQIWRgUGVkXz9gkAsRIH9+kBsQIH+UTCyAQD9bQDO"
+            }
+          }
+        }
+      }
+    }
+  }
+}
