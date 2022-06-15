6e614e376111111114bc0086337ea863717ab418
IoriBranford <ioribranford@gmail.com>
2022-06-14 21:05:34 -0700

    Put player tile shapes in all of her tiles.

--- data/tilesets/Jenny.tsx
+++ data/tilesets/Jenny.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Jenny" tilewidth="32" tileheight="32" tilecount="3" columns="3" objectalignment="center">
+<tileset version="1.8" tiledversion="1.8.4" name="Jenny" tilewidth="32" tileheight="32" tilecount="3" columns="3" objectalignment="center">
  <image source="Jenny.png" width="96" height="32"/>
  <tile id="0">
   <properties>
@@ -24,6 +24,16 @@
   <properties>
    <property name="name" value="flyslow"/>
   </properties>
+  <objectgroup>
+   <object id="1" name="sting" x="16" y="28">
+    <point/>
+   </object>
+   <object id="2" name="hitbox" x="12" y="12" width="8" height="8">
+    <properties>
+     <property name="ishitbox" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
   <animation>
    <frame tileid="0" duration="100"/>
    <frame tileid="1" duration="100"/>
