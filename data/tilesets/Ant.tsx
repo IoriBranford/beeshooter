01dee21099095b09a66ba90c016206bcbd342912
IoriBranford <ioribranford@gmail.com>
2022-06-14 17:55:01 -0700

    Move tilesets into data/tilesets.

---
+++ data/tilesets/Ant.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Ant" tilewidth="16" tileheight="16" tilecount="2" columns="2" objectalignment="center">
+ <image source="Ant.png" width="32" height="16"/>
+ <tile id="0" type="Ant">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="0" y="2" width="16" height="12"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1" type="Ant"/>
+</tileset>
---
+++ data/tilesets/BeehiveObjects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="BeehiveObjects" tilewidth="16" tileheight="32" tilecount="12" columns="12" objectalignment="center">
+ <image source="BeehiveObjects.png" width="192" height="32"/>
+</tileset>
