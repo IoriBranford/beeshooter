ee371b173a9915a05265ce02e39e55b45d705e4c
IoriBranford <ioribranford@gmail.com>
2022-06-16 14:54:13 -0700

    Fly sprite assets.

---
+++ data/tilesets/FlyBullet.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="FlyBullet" tilewidth="16" tileheight="8" tilecount="2" columns="2" objectalignment="center">
+ <image source="FlyBullet.png" width="32" height="8"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="6" y="2" width="4" height="4"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
