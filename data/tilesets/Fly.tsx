e137b68a5b8df6489a3734ada6e9bc8392f4d25d
IoriBranford <ioribranford@gmail.com>
2022-06-16 14:54:13 -0700

    Fly sprite assets.

---
+++ data/tilesets/Fly.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Fly" tilewidth="32" tileheight="32" tilecount="16" columns="16" objectalignment="center">
+ <image source="Fly.png" width="512" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="4" y="6" width="24" height="20"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="14" duration="50"/>
+   <frame tileid="15" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
