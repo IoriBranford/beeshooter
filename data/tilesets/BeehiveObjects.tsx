bfe17c60a6ac66c23e73eb357aae08d2188461b9
IoriBranford <ioribranford@gmail.com>
2022-06-16 11:52:53 -0700

    Add hitboxes to honeypot and powerup.

--- data/tilesets/BeehiveObjects.tsx
+++ data/tilesets/BeehiveObjects.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.5" name="BeehiveObjects" tilewidth="16" tileheight="32" tilecount="12" columns="12" objectalignment="center">
  <image source="BeehiveObjects.png" width="192" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="0" y="12" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="0" y="16" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="0" y="6" width="16" height="20"/>
+  </objectgroup>
+ </tile>
 </tileset>
