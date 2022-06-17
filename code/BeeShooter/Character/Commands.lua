7c33c503bffe62ac4c168d6ead33b157011b443b
IoriBranford <ioribranford@gmail.com>
2022-06-17 15:23:10 -0700

    Implement enemy shoot commands.

--- code/BeeShooter/Character/Commands.lua
+++ code/BeeShooter/Character/Commands.lua
@@ -1,4 +1,5 @@
 local Config   = require "System.Config"
+local Stage
 
 local setfenv = setfenv
 local wait, waitfor = coroutine.wait, coroutine.waitfor
@@ -15,6 +16,55 @@ local Env = {
 local EnvMetatable = {}
 setmetatable(Env, EnvMetatable)
 local self
+
+local pi, cos, sin = math.pi, math.cos, math.sin
+local atan2 = math.atan2
+local norm = math.norm
+
+function Env.setSpeed(speed)
+    self.speed = speed
+end
+
+function Env.shootAS(bullettype, angle, speed)
+    angle = angle or 0
+    speed = speed or 1
+    local velx, vely = cos(angle)*speed, sin(angle)*speed
+    Stage.addCharacter({
+        type = bullettype,
+        x = self.x,
+        y = self.y,
+        rotation = angle,
+        velx = velx,
+        vely = vely
+    })
+end
+local shootAS = Env.shootAS
+
+function Env.shootXY(bullettype, velx, vely)
+    velx, vely = velx or 0, vely or 0
+    local angle = velx == 0 and vely == 0 and 0 or atan2(vely, velx)
+    Stage.addCharacter({
+        type = bullettype,
+        x = self.x,
+        y = self.y,
+        rotation = angle,
+        velx = velx,
+        vely = vely
+    })
+end
+local shootXY = Env.shootXY
+
+function Env.shootTargetAS(bullettype, target, angle, speed)
+    if not target then
+        return
+    end
+    local x, y = self.x, self.y
+    local targetx, targety = target.x, target.y
+    local angleoffset = targetx == x and targety == y and 0 or atan2(targety - y, targetx - x)
+    angle = (angle or 0) + angleoffset
+    speed = speed or 1
+    shootAS(bullettype, angle, speed)
+end
 
 function Env.setScaleX(sx)
     local sprite = self.sprite
@@ -37,6 +87,7 @@ end
 local Commands = {}
 
 function Commands.setEnvOn(commandscript)
+    Stage    = require "BeeShooter.Stage"
     setfenv(commandscript, Env)
     Env.difficulty = Config.game_difficulty
 end
