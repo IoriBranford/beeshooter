43cde4ab3e25a20775a990c57b10cedf060a6875
IoriBranford <ioribranford@gmail.com>
2022-06-18 09:02:56 -0700

    Alien prefabs and data.

--- data/prefabs_caravan.lua
+++ data/prefabs_caravan.lua
@@ -9,7 +9,7 @@ return {
   tilewidth = 16,
   tileheight = 16,
   nextlayerid = 6,
-  nextobjectid = 10,
+  nextobjectid = 12,
   properties = {},
   tilesets = {
     {
@@ -424,6 +424,222 @@ return {
           }
         }
       }
+    },
+    {
+      name = "Aliens",
+      firstgid = 22,
+      tilewidth = 24,
+      tileheight = 16,
+      spacing = 0,
+      margin = 0,
+      columns = 6,
+      image = "tilesets/Aliens.png",
+      imagewidth = 144,
+      imageheight = 32,
+      objectalignment = "center",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 24,
+        height = 16
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 12,
+      tiles = {
+        {
+          id = 0,
+          properties = {
+            ["name"] = "gun_walk"
+          },
+          objectGroup = {
+            type = "objectgroup",
+            draworder = "index",
+            id = 2,
+            name = "",
+            visible = true,
+            opacity = 1,
+            offsetx = 0,
+            offsety = 0,
+            parallaxx = 1,
+            parallaxy = 1,
+            properties = {},
+            objects = {
+              {
+                id = 1,
+                name = "hitbox",
+                type = "",
+                shape = "rectangle",
+                x = 2,
+                y = 2,
+                width = 20,
+                height = 12,
+                rotation = 0,
+                visible = true,
+                properties = {}
+              }
+            }
+          },
+          animation = {
+            {
+              tileid = 0,
+              duration = 100
+            },
+            {
+              tileid = 1,
+              duration = 100
+            },
+            {
+              tileid = 2,
+              duration = 100
+            },
+            {
+              tileid = 3,
+              duration = 100
+            }
+          }
+        },
+        {
+          id = 4,
+          properties = {
+            ["name"] = "gun_stand"
+          },
+          objectGroup = {
+            type = "objectgroup",
+            draworder = "topdown",
+            name = "",
+            visible = true,
+            opacity = 1,
+            offsetx = 0,
+            offsety = 0,
+            parallaxx = 1,
+            parallaxy = 1,
+            properties = {},
+            objects = {
+              {
+                id = 1,
+                name = "hitbox",
+                type = "",
+                shape = "rectangle",
+                x = 2,
+                y = 2,
+                width = 20,
+                height = 12,
+                rotation = 0,
+                visible = true,
+                properties = {}
+              }
+            }
+          },
+          animation = {
+            {
+              tileid = 4,
+              duration = 100
+            },
+            {
+              tileid = 5,
+              duration = 100
+            }
+          }
+        },
+        {
+          id = 6,
+          properties = {
+            ["name"] = "claw_walk"
+          },
+          objectGroup = {
+            type = "objectgroup",
+            draworder = "topdown",
+            name = "",
+            visible = true,
+            opacity = 1,
+            offsetx = 0,
+            offsety = 0,
+            parallaxx = 1,
+            parallaxy = 1,
+            properties = {},
+            objects = {
+              {
+                id = 1,
+                name = "hitbox",
+                type = "",
+                shape = "rectangle",
+                x = 2,
+                y = 2,
+                width = 20,
+                height = 12,
+                rotation = 0,
+                visible = true,
+                properties = {}
+              }
+            }
+          },
+          animation = {
+            {
+              tileid = 6,
+              duration = 100
+            },
+            {
+              tileid = 7,
+              duration = 100
+            },
+            {
+              tileid = 8,
+              duration = 100
+            },
+            {
+              tileid = 9,
+              duration = 100
+            }
+          }
+        },
+        {
+          id = 10,
+          properties = {
+            ["name"] = "claw_scrape"
+          },
+          objectGroup = {
+            type = "objectgroup",
+            draworder = "topdown",
+            name = "",
+            visible = true,
+            opacity = 1,
+            offsetx = 0,
+            offsety = 0,
+            parallaxx = 1,
+            parallaxy = 1,
+            properties = {},
+            objects = {
+              {
+                id = 1,
+                name = "hitbox",
+                type = "",
+                shape = "rectangle",
+                x = 2,
+                y = 2,
+                width = 20,
+                height = 12,
+                rotation = 0,
+                visible = true,
+                properties = {}
+              }
+            }
+          },
+          animation = {
+            {
+              tileid = 10,
+              duration = 100
+            },
+            {
+              tileid = 11,
+              duration = 100
+            }
+          }
+        }
+      }
     }
   },
   layers = {
@@ -598,6 +814,34 @@ return {
           properties = {
             ["z"] = 1
           }
+        },
+        {
+          id = 10,
+          name = "AlienGunner",
+          type = "AlienGunner",
+          shape = "rectangle",
+          x = 0,
+          y = 0,
+          width = 24,
+          height = 16,
+          rotation = 0,
+          gid = 22,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 11,
+          name = "AlienPillager",
+          type = "AlienPillager",
+          shape = "rectangle",
+          x = 0,
+          y = 0,
+          width = 24,
+          height = 16,
+          rotation = 0,
+          gid = 28,
+          visible = true,
+          properties = {}
         }
       }
     }
