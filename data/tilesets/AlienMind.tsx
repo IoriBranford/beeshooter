ffe9628093a9c9f54f7109d17e412e7c0644bd49
IoriBranford <ioribranford@gmail.com>
2022-06-18 09:59:16 -0700

    Alien mind sprite assets.

---
+++ data/tilesets/AlienMind.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="AlienMind" tilewidth="32" tileheight="48" tilecount="4" columns="4" objectalignment="center">
+ <image source="AlienMind.png" width="128" height="48"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="4" y="8" width="24" height="32"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
