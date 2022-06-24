e3617a73621f39de3229cc3232b8996b1475d1b2
IoriBranford <ioribranford@gmail.com>
2022-06-23 21:46:39 -0700

    Second pass at common death effect.

---
+++ data/tilesets/KillSmall.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="KillSmall" tilewidth="32" tileheight="32" tilecount="8" columns="8" objectalignment="center">
+ <image source="KillSmall.png" width="256" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="16"/>
+   <frame tileid="1" duration="16"/>
+   <frame tileid="2" duration="16"/>
+   <frame tileid="3" duration="16"/>
+   <frame tileid="4" duration="16"/>
+   <frame tileid="5" duration="16"/>
+   <frame tileid="6" duration="16"/>
+   <frame tileid="7" duration="16"/>
+  </animation>
+ </tile>
+</tileset>
