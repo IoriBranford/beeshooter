56e39289b82b7e9ee7b2494f6861e39c90108f61
IoriBranford <ioribranford@gmail.com>
2022-06-26 14:21:13 -0700

    Additional player death effect sprite.

---
+++ data/tilesets/JennyKill.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="JennyKill" tilewidth="64" tileheight="64" tilecount="13" columns="13" objectalignment="bottom">
+ <image source="JennyKill.png" width="832" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
