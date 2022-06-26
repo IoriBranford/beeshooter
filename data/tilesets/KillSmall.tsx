811c4cd9fa4bf3b16787df7ad414fe519540a996
IoriBranford <ioribranford@gmail.com>
2022-06-26 14:03:53 -0700

    Medium kill prefab. Improve small kill sprite and set lifetime to animation time.

--- data/tilesets/KillSmall.tsx
+++ data/tilesets/KillSmall.tsx
@@ -1,16 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="KillSmall" tilewidth="32" tileheight="32" tilecount="8" columns="8" objectalignment="center">
- <image source="KillSmall.png" width="256" height="32"/>
+<tileset version="1.9" tiledversion="1.9.0" name="KillSmall" tilewidth="32" tileheight="32" tilecount="15" columns="15" objectalignment="bottom">
+ <image source="KillSmall.png" width="480" height="32"/>
  <tile id="0">
   <animation>
-   <frame tileid="0" duration="16"/>
-   <frame tileid="1" duration="16"/>
-   <frame tileid="2" duration="16"/>
-   <frame tileid="3" duration="16"/>
-   <frame tileid="4" duration="16"/>
-   <frame tileid="5" duration="16"/>
-   <frame tileid="6" duration="16"/>
-   <frame tileid="7" duration="16"/>
+   <frame tileid="0" duration="33"/>
+   <frame tileid="1" duration="33"/>
+   <frame tileid="2" duration="33"/>
+   <frame tileid="3" duration="33"/>
+   <frame tileid="4" duration="33"/>
+   <frame tileid="5" duration="33"/>
+   <frame tileid="6" duration="33"/>
+   <frame tileid="7" duration="33"/>
+   <frame tileid="8" duration="33"/>
+   <frame tileid="9" duration="33"/>
+   <frame tileid="10" duration="33"/>
+   <frame tileid="11" duration="33"/>
+   <frame tileid="12" duration="33"/>
+   <frame tileid="13" duration="33"/>
+   <frame tileid="14" duration="33"/>
   </animation>
  </tile>
 </tileset>
