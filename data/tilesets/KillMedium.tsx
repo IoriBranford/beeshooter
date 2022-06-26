5cf17b407d7643e132b8c60a70044746ae5647c6
IoriBranford <ioribranford@gmail.com>
2022-06-26 14:02:53 -0700

    Medium kill sprite.

---
+++ data/tilesets/KillMedium.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="KillMedium" tilewidth="64" tileheight="64" tilecount="16" columns="16" objectalignment="bottom">
+ <image source="KillMedium.png" width="1024" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="14" duration="50"/>
+   <frame tileid="15" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
