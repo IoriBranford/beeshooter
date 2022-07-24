260ceee41462affcba76edeec48494a1925dca74
IoriBranford <ioribranford@gmail.com>
2022-07-23 18:08:40 -0700

    Create wasp enemy. Attack TBD.

---
+++ data/tilesets/Wasp.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Wasp" tilewidth="40" tileheight="30" tilecount="2" columns="2" objectalignment="center">
+ <image source="Wasp.png" width="80" height="30"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="8" y="8" width="24" height="12"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
