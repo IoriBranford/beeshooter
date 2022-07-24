e1550f0775db3eec1d00aa4bc9682a152cfd9d4b
IoriBranford <ioribranford@gmail.com>
2022-07-23 18:41:44 -0700

    Create Beetle enemy.

---
+++ data/tilesets/Beetle.tsx
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Beetle" tilewidth="48" tileheight="48" tilecount="20" columns="5" objectalignment="center">
+ <image source="Beetle.png" width="240" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <objectgroup>
+   <object id="1" name="hitbox" x="12" y="12" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+</tileset>
