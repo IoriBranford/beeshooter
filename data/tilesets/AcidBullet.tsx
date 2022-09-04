367d8720b5d223710386cb8aee19aa61d6173ad2
IoriBranford <ioribranford@gmail.com>
2022-09-03 18:20:10 -0700

    Add acid bullet assets.

---
+++ data/tilesets/AcidBullet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="AcidBullet" tilewidth="16" tileheight="8" tilecount="2" columns="2" objectalignment="right">
+ <tileoffset x="3" y="0"/>
+ <image source="AcidBullet.png" width="32" height="8"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="hitbox" x="11" y="2" width="4" height="4"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
