b1686278fcdc4cbc59a89c74ed6637cfe57662c3
IoriBranford <ioribranford@gmail.com>
2022-09-16 15:45:51 -0700

    Correct variable declaration

--- serve-web.sh
+++ serve-web.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$WEB_DIR=${WEB_DIR:=public}
+WEB_DIR=${WEB_DIR:=public}
 
 npm install local-web-server
 npx ws --directory $WEB_DIR --cors.opener-policy same-origin --cors.embedder-policy require-corp
