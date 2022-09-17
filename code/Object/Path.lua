13e862817c1f0bb8c978b2104b74f384cc4fa321
IoriBranford <ioribranford@gmail.com>
2022-09-16 17:07:08 -0700

    Alternate path calc functions which do not mix with pathpoint scripting but may be useful later.

--- code/Object/Path.lua
+++ code/Object/Path.lua
@@ -34,6 +34,98 @@ function Path.getNextIndex(path, i, di)
     return i
 end
 
+---Precalc data for one-dimensional path progress
+function Path.calcLengths(path)
+    local startslengths = path.startslengths
+    if startslengths then
+        return
+    end
+    startslengths = {}
+    local points = path.points
+    local x, y = points[1], points[2]
+    local totallength = 0;
+    for i = 2, #points-2, 2 do
+        local x2, y2 = points[i+1], points[i+2]
+        local seglength = math.dist(x, y, x2, y2)
+        startslengths[i-1] = totallength
+        startslengths[i] = seglength
+        x, y = x2, y2
+        totallength = totallength+seglength
+        print(i, totallength, seglength)
+    end
+    startslengths[#points-1] = totallength
+    startslengths[#points] = 0
+    path.startslengths = startslengths
+    path.totallength = totallength
+end
+
+function Path.clampIndex(path, i)
+    if i < 2 then
+        return 2
+    end
+    if i > #path.points then
+        return #path.points
+    end
+    return i
+end
+
+function Path.clampPosition1d(path, pos)
+    if pos < 0 then
+        return 0
+    end
+    if pos > path.totallength then
+        return path.totallength
+    end
+    return pos
+end
+
+function Path.updatePosition1d(path, i, pos, speed)
+    i = Path.clampIndex(path, i)
+    pos = pos + speed
+    pos = Path.clampPosition1d(path, pos)
+    local points = path.points
+    local startslengths = path.startslengths
+
+    if speed > 0 then
+        while i <= #points do
+            local segstart = startslengths[i-1]
+            local seglength = startslengths[i]
+            if pos >= segstart + seglength then
+                i = i + 2
+            else
+                break
+            end
+        end
+    elseif speed < 0 then
+        while i >= 2 do
+            local segstart = startslengths[i-1]
+            if pos <= segstart then
+                i = i - 2
+            else
+                break
+            end
+        end
+    end
+    return i, pos
+end
+
+function Path.getPosition2d(path, i, pos)
+    i = Path.clampIndex(path, i)
+    pos = Path.clampPosition1d(path, pos)
+    local points = path.points
+    local startslengths = path.startslengths
+    local segx1, segy1 = points[i-1], points[i]
+    local segstart, seglength = startslengths[i-1], startslengths[i]
+    if seglength <= 0 then
+        return segx1, segy1
+    end
+    local segx2, segy2 = points[i+1] or segx1, points[i+2] or segy1
+    local segdx, segdy = segx2 - segx1, segy2 - segy1
+    local segpos = pos - segstart
+    local dirx, diry = segdx / seglength, segdy / seglength
+    return segx1 + segpos*dirx, segy1 + segpos*diry
+end
+
 function Path.getSegmentLengthSq(path, endi)
     if endi < 4 or endi > #path then
         return 0
