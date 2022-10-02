6b729fb5091661b902afb43a30e47bf3fb4c44ed
IoriBranford <ioribranford@gmail.com>
2022-10-01 17:08:09 -0700

    Make weapons tileset.

---
+++ data/tilesets/Weapons.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Weapons" tilewidth="16" tileheight="16" tilecount="8" columns="8">
+ <image source="Weapons.png" width="128" height="16"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
