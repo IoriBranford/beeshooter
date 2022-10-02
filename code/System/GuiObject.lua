ca91421199367ebfdb7064d257bf4c896ebd59dd
IoriBranford <ioribranford@gmail.com>
2022-10-01 19:02:28 -0700

    Add setColor and setPosition to GuiObject. Keep GuiObject string equal to its scene object string.

--- code/System/GuiObject.lua
+++ code/System/GuiObject.lua
@@ -15,7 +15,18 @@ function GuiObject:changeTile(tileid)
 end
 
 function GuiObject:setString(string)
+    self.string = string
     self.sprite:setTextString(string)
+end
+
+function GuiObject:setColor(r, g, b, a)
+    self.sprite:setColor(r, g, b, a)
+end
+
+function GuiObject:setPosition(x, y)
+    self.x, self.y = x, y
+    self.sprite.x = x
+    self.sprite.y = y
 end
 
 return GuiObject
