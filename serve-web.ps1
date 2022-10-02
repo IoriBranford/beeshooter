b5ed49f787cff28198b783e04bbec4594c21ba5b
IoriBranford <ioribranford@gmail.com>
2022-10-02 13:22:46 -0700

    Serve https.

--- serve-web.ps1
+++ serve-web.ps1
@@ -2,4 +2,4 @@ param(
     $WebDir="public"
 )
 
-npx ws --directory $WebDir --cors.opener-policy same-origin --cors.embedder-policy require-corp
+npx ws --directory $WebDir --https --cors.opener-policy same-origin --cors.embedder-policy require-corp
