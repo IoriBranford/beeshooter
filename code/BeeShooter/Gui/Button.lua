bbcd5b233f8f023b82065a4e1ce1bf1ab58b0a11
IoriBranford <ioribranford@gmail.com>
2022-12-07 19:32:45 -0800

    Make game-specific Button class a proper class.

--- code/BeeShooter/Gui/Button.lua
+++ code/BeeShooter/Gui/Button.lua
@@ -1,17 +1,9 @@
-local tablex     = require "pl.tablex"
 local BaseButton    = require "Gui.Button"
 local Stage        = require "BeeShooter.Stage"
 local GamePhase    = require "BeeShooter.GamePhase"
+local class        = require "pl.class"
 
-local Button = tablex.copy(BaseButton)
-Button.__index = Button
-
-function Button:init()
-    setmetatable(self, Button)
-
-    local action = self.action
-    self.action = Button[action] or Button.playInvalidSound
-end
+local Button = class(BaseButton)
 
 function Button:resumeGame()
     GamePhase.setPaused(false)
