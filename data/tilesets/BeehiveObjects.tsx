1b5cb7e6f2380699fe04ce44377296a5b609df31
IoriBranford <ioribranford@gmail.com>
2022-12-17 15:44:42 -0800

    Remove flashing animation from silver honey pot sprite.

--- data/tilesets/BeehiveObjects.tsx
+++ data/tilesets/BeehiveObjects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="BeehiveObjects" tilewidth="16" tileheight="32" tilecount="12" columns="12" objectalignment="center">
+<tileset version="1.9" tiledversion="1.9.2" name="BeehiveObjects" tilewidth="16" tileheight="32" tilecount="12" columns="12" objectalignment="center">
  <image source="BeehiveObjects.png" width="192" height="32"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
@@ -10,19 +10,11 @@
   <objectgroup draworder="index" id="2">
    <object id="1" name="hitbox" x="0" y="12" width="16" height="16"/>
   </objectgroup>
-  <animation>
-   <frame tileid="2" duration="50"/>
-   <frame tileid="0" duration="50"/>
-  </animation>
  </tile>
  <tile id="3">
   <objectgroup draworder="index" id="2">
    <object id="1" name="hitbox" x="0" y="12" width="16" height="16"/>
   </objectgroup>
-  <animation>
-   <frame tileid="3" duration="50"/>
-   <frame tileid="1" duration="50"/>
-  </animation>
  </tile>
  <tile id="4">
   <objectgroup draworder="index" id="2">
