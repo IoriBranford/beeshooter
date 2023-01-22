6978a1fb5ba978ba5fc1cf3a2c97666713e9c983
IoriBranford <ioribranford@gmail.com>
2023-01-22 15:53:37 -0800

    Make cursor sprite for touch movement.

---
+++ data/tilesets/MoveCursor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="MoveCursor" tilewidth="8" tileheight="8" tilecount="1" columns="1" objectalignment="center">
+ <image source="MoveCursor.png" width="8" height="8"/>
+</tileset>
