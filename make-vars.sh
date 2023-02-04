7ec1c689e94089826738d9b7d7c41666dbfb05ea
IoriBranford <ioribranford@gmail.com>
2023-02-03 23:29:24 -0800

    Make project title vars available to CI so it can find artifact files.

--- make-vars.sh
+++ make-vars.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 export PROJECT=honeysoldier
-export PROJECT_TITLE="Honey Soldier"
-export PROJECT_TITLE_NOSPACE=Honey_Soldier
+export PROJECT_TITLE=${PROJECT_TITLE:="Honey Soldier"}
+export PROJECT_TITLE_NOSPACE=${PROJECT_TITLE_NOSPACE:=Honey_Soldier}
 export DESCRIPTION="Caravan shooting"
 export COPYRIGHT="© 2022-2023 IoriBranford"
-export APPLICATION_ID_BASE="com.ioribranford.honeysoldier"
+export APPLICATION_ID="com.ioribranford.honeysoldier"
 export LOVE_VERSION="11.4"
-export SCREEN_ORIENTATION="sensorLandscape"
+export SCREEN_ORIENTATION="userLandscape"
