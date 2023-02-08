e0bb8dc4136e009ef7918bf6303f8301556cb4a4
IoriBranford <ioribranford@gmail.com>
2023-02-07 21:16:33 -0800

    Add splat decal sprites.

---
+++ data/tilesets/Splats.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Splats" tilewidth="24" tileheight="24" tilecount="36" columns="6">
+ <image source="Splats.png" width="144" height="144"/>
+</tileset>
