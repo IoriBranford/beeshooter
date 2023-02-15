583a061e28707c16d459751ced9436f812029b34
IoriBranford <ioribranford@gmail.com>
2023-02-14 17:08:21 -0800

    Only access cursor offsets in cursor code.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

--- code/Gui/Cursor.lua
+++ code/Gui/Cursor.lua
@@ -6,9 +6,17 @@ local Cursor = class(GuiObject)
 Cursor.iscursor = true
 
 function Cursor:init()
+    self.offsetx = self.offsetx or 0
+    self.offsety = self.offsety or 0
 end
 
 function Cursor:onSelect(i, item)
+end
+
+function Cursor:moveTo(item)
+    self:setPosition(
+        item.x + self.offsetx,
+        item.y + self.offsety)
 end
 
 function Cursor:onMoveTo(i, item)
