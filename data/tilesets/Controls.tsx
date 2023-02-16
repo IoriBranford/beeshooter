74c1d7a8988402c52b07baf880065e179e996999
IoriBranford <ioribranford@gmail.com>
2023-02-16 15:29:30 -0800

    Add Kenney input icon tileset.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

---
+++ data/tilesets/Controls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Controls" tilewidth="16" tileheight="16" tilecount="900" columns="36">
+ <image source="Controls.png" width="578" height="408"/>
+</tileset>
