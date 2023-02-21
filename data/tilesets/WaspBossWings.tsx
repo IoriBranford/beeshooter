3dedab6bf05a5239fc7aad7a949428e779aa7afc
IoriBranford <ioribranford@gmail.com>
2023-02-20 17:19:26 -0800

    Wasp boss screams and explodes into pieces.

---
+++ data/tilesets/WaspBossWings.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="WaspBossWings" tilewidth="64" tileheight="64" tilecount="3" columns="3" objectalignment="center">
+ <image source="WaspBossWings.png" width="192" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
