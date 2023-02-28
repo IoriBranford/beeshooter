5221853361465453b1fe1fa9a020a5366c025ad1
IoriBranford <ioribranford@gmail.com>
2023-02-28 12:59:51 -0800

    Fix make-web.sh only getting the first word of the title.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

--- make-web.sh
+++ make-web.sh
@@ -9,4 +9,4 @@ GAME_ASSET=${GAME_ASSET:=${GAME_TYPE}.love}
 WEB_DIR=public
 
 ./make-game.sh
-npx love.js ${GAME_ASSET} ${WEB_DIR} -t ${PROJECT_TITLE}
+npx love.js ${GAME_ASSET} ${WEB_DIR} -t "${PROJECT_TITLE}"
