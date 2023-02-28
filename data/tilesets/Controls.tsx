38e0238582138dd7bb7cf217a3f7061b469b4d98
IoriBranford <ioribranford@gmail.com>
2023-02-28 15:16:13 -0800

    Keyboard and gamepad icon sprites.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

--- data/tilesets/Controls.tsx
+++ data/tilesets/Controls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Controls" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="Controls.png" width="16" height="16"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Controls" tilewidth="16" tileheight="16" tilecount="19" columns="19" objectalignment="topleft">
+ <image source="Controls.png" width="304" height="16"/>
 </tileset>
