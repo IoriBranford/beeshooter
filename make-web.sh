33311b9224e7281e7c69e37b0614999ed66bf5a2
IoriBranford <ioribranford@gmail.com>
2023-02-28 16:14:15 -0800

    Do not give web build a title.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

--- make-web.sh
+++ make-web.sh
@@ -2,11 +2,9 @@
 
 . ./make-vars.sh
 
-PROJECT=${PROJECT:=${PWD##*/}}
-PROJECT_TITLE=${PROJECT_TITLE:=${PROJECT}${GAME_TYPE}}
 GAME_TYPE=${GAME_TYPE:=game}
 GAME_ASSET=${GAME_ASSET:=${GAME_TYPE}.love}
 WEB_DIR=public
 
 ./make-game.sh
-npx love.js ${GAME_ASSET} ${WEB_DIR} -t "${PROJECT_TITLE}"
+npx love.js ${GAME_ASSET} ${WEB_DIR} -t " "
