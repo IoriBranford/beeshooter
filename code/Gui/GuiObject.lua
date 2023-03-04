2a77164f097fe31c732b1003f20dddc13c668043
IoriBranford <ioribranford@gmail.com>
2023-03-04 15:38:59 -0800

    GuiObject anchor code.
    Signed-off-by: IoriBranford <ioribranford@gmail.com>

--- code/Gui/GuiObject.lua
+++ code/Gui/GuiObject.lua
@@ -5,6 +5,27 @@ local class = require "pl.class"
 local GuiObject = class()
 
 function GuiObject:init()
+    local points = self.points
+    if points then
+        local x1, y1, x2, y2 = math.huge, math.huge, -math.huge, -math.huge
+        for i = 2, #points, 2 do
+            local x, y = points[i-1], points[i]
+            x1 = math.min(x1, x)
+            y1 = math.min(y1, y)
+            x2 = math.max(x2, x)
+            y2 = math.max(y2, y)
+        end
+        self.leftx = self.x + x1
+        self.topy = self.y + y1
+        self.width = x2-x1
+        self.height = y2-y1
+    elseif self.tile then
+        self.leftx = self.x - self.tile.objectoriginx
+        self.topy = self.y - self.tile.objectoriginy
+    else
+        self.leftx, self.topy = self.x, self.y
+    end
+    self.x0, self.y0 = self.x, self.y
 end
 
 function GuiObject:changeTile(tileid)
@@ -19,6 +40,10 @@ end
 
 function GuiObject:setColor(r, g, b, a)
     self.sprite:setColor(r, g, b, a)
+end
+
+function GuiObject:resetPosition()
+    self:setPosition(self.x0, self.y0)
 end
 
 function GuiObject:translate(dx, dy)
@@ -52,6 +77,18 @@ end
 function GuiObject:setHidden(hidden)
     self.visible = not hidden
     self:setSpriteHidden(hidden)
+end
+
+function GuiObject:reanchor(guiwidth, guiheight, screenwidth, screenheight)
+    local anchorx = self.anchorx or 0
+    local anchory = self.anchory or 0
+    local dx = anchorx * (screenwidth-guiwidth)/2
+    local dy = anchory * (screenheight-guiheight)/2
+    self:resetPosition()
+    self:translate(dx, dy)
+    for i = 1, #self do
+        self[i]:reanchor(guiwidth, guiheight, screenwidth, screenheight)
+    end
 end
 
 return GuiObject
