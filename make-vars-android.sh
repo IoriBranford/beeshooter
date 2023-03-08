eff46a6a8d20ac3dc047a14a02f17cef6399d9ee
IoriBranford <ioribranford@gmail.com>
2023-03-08 13:15:17 -0800

    Android versionCode 7.

--- make-vars-android.sh
+++ make-vars-android.sh
@@ -6,4 +6,4 @@ export APPLICATION_ID="com.ioribranford.honeysoldier"
 export ANDROID_ICON="@mipmap/ic_launcher"
 export ANDROID_ACTIVITY="${APPLICATION_ID}.GameActivity"
 export ANDROID_VERSIONNAME="`git describe --tags --always`"
-export ANDROID_VERSIONCODE=6
+export ANDROID_VERSIONCODE=7
