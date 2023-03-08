38c7fd8554138b1509df86055dd95d43e9a63d9c
IoriBranford <ioribranford@gmail.com>
2023-03-08 13:14:57 -0800

    ANDROID_ICON, ANDROID_ACTIVITY, and ANDROID_VERSIONNAME.

--- make-vars-android.sh
+++ make-vars-android.sh
@@ -3,4 +3,7 @@
 export PROJECT_TITLE="Honey Soldier"
 export PROJECT_TITLE_NOSPACE=Honey_Soldier
 export APPLICATION_ID="com.ioribranford.honeysoldier"
+export ANDROID_ICON="@mipmap/ic_launcher"
+export ANDROID_ACTIVITY="${APPLICATION_ID}.GameActivity"
+export ANDROID_VERSIONNAME="`git describe --tags --always`"
 export ANDROID_VERSIONCODE=6
