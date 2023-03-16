b000e5ebbe0c1e17077c9b11b33579d48bbde1a1
IoriBranford <ioribranford@gmail.com>
2023-03-15 22:40:18 -0700

    Update docker-make-android to use full image.

--- docker-make-android.sh
+++ docker-make-android.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-. ./make-vars.sh
+. ./make-vars-android.sh
 
-docker run -i -t --rm \
-	-v $(pwd):$(pwd) -w $(pwd) --user $(id -u):$(id -g) \
+docker run --rm \
+	-v $GAME_DIR:/game:ro \
+	-v ./outputs:/love-android/app/build/outputs \
+	-e KEYSTORE_FILE="/game/release.keystore" \
 	-e KEYSTORE_ALIAS -e KEYSTORE_PASSWORD \
-	-e GRADLE_USER_HOME="$(pwd)/.gradle" \
-	ioribranford/build-love-android:$LOVE_VERSION \
-	bash -c "./setup-android.sh && ./make-android.sh"
+	-e APPLICATION_ID \
+	-e GAME_TITLE \
+	-e VERSION_NAME \
+	-e VERSION_CODE \
+	-e ICON \
+	-v $ICONS_DIR:/love-android/app/src/main/res:ro \
+	ioribranford/build-love-android:$LOVE_VERSION-full 
+	# sh -c "./setup-android.sh && ./make-android.sh"
