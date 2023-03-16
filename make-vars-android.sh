ed5a9275e0ac4015c063dec1af8c41b5727f2a0b
IoriBranford <ioribranford@gmail.com>
2023-03-16 00:04:04 -0700

    Add KEYSTORE_FILE var.

--- make-vars-android.sh
+++ make-vars-android.sh
@@ -7,5 +7,6 @@ export GAME_TITLE_NOSPACE=Honey_Soldier
 export APPLICATION_ID="com.ioribranford.honeysoldier"
 export ICON="@mipmap/ic_launcher"
 export ICONS_DIR="$PWD/android/app/src/main/res"
+export KEYSTORE_FILE="$PWD/release.keystore"
 export VERSION_NAME="`git describe --tags --always`"
 export VERSION_CODE=7
