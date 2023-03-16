648d186716e371480297fcdddeaa5ad6e31e2bf1
IoriBranford <ioribranford@gmail.com>
2023-03-16 00:05:56 -0700

    Make docker-make-android work more like Github workflow.

--- docker-make-android.sh
+++ docker-make-android.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-. ./make-vars-android.sh
+. ./make-vars.sh
 
 docker run --rm \
-	-v $GAME_DIR:/game:ro \
+	-v $(pwd):$(pwd) -w $(pwd) \
 	-v ./outputs:/love-android/app/build/outputs \
-	-e KEYSTORE_FILE="/game/release.keystore" \
 	-e KEYSTORE_ALIAS -e KEYSTORE_PASSWORD \
-	-e APPLICATION_ID \
-	-e GAME_TITLE \
-	-e VERSION_NAME \
-	-e VERSION_CODE \
-	-e ICON \
-	-v $ICONS_DIR:/love-android/app/src/main/res:ro \
-	ioribranford/build-love-android:$LOVE_VERSION-full 
-	# sh -c "./setup-android.sh && ./make-android.sh"
+	ioribranford/build-love-android:$LOVE_VERSION-full \
+	sh -c '. ./make-vars-android.sh && cd /love-android && ./build.sh'
