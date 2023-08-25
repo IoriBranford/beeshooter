56e6b202d96c2e95a84919601dc93a1a0e33201e
IoriBranford <ioribranford@gmail.com>
2023-08-25 11:18:38 -0700

    Don't depend on git for the version name.

--- make-vars-android.sh
+++ make-vars-android.sh
@@ -8,5 +8,5 @@ export APPLICATION_ID="com.ioribranford.honeysoldier"
 export ICON="@mipmap/ic_launcher"
 export ICONS_DIR="$PWD/android/app/src/main/res"
 export KEYSTORE_FILE="$PWD/release.keystore"
-export VERSION_NAME="`git describe --tags --always`"
+export VERSION_NAME="android-8"
 export VERSION_CODE=8
