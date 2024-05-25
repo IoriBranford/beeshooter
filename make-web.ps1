5262907df6e9de19a70a7d898da65aa3e6028561
IoriBranford <ioribranford@gmail.com>
2024-05-24 22:09:37 -0700

    Switch to github.com/2dengine/love.js for the Web build.

--- make-web.ps1
+++ make-web.ps1
@@ -6,5 +6,25 @@ param(
 
 ./make-game.ps1 -GameType=$GameType -GameAsset=$GameAsset
 
-npx love.js.cmd $GameAsset $WebDir -t " "
-Compress-Archive -Path "$WebDir\*" -DestinationPath "$WebDir.zip" -Force
+$LoveJsVersion="0d78d942635570899ab81ffa9f00e70433a177af"
+$LoveJsZip="${LoveJsVersion}.zip"
+$LoveJsUrl="https://github.com/2dengine/love.js/archive/${LoveJsZip}"
+$LoveVersion="11.5"
+
+if (-not (Test-Path $LoveJsZip)) {
+    curl -Lk -o $LoveJsZip $LoveJsUrl
+}
+
+if (-not (Test-Path love.js-$LoveJsVersion)) {
+    Expand-Archive $LoveJsZip .
+}
+
+if (Test-Path $WebDir) {
+    Remove-Item -Force -Recurse $WebDir
+}
+
+mkdir $WebDir\play
+Copy-Item -Recurse love.js-$LoveJsVersion\$LoveVersion\release $WebDir\play\$LoveVersion\release
+Copy-Item love.js-$LoveJsVersion\* $WebDir\play -Include *.js,*.css
+Copy-Item love.js-$LoveJsVersion\index.html $WebDir\index.html 
+Copy-Item $GameAsset $WebDir\play\nogame.love
--- make-web.sh
+++ make-web.sh
@@ -7,4 +7,25 @@ GAME_ASSET=${GAME_ASSET:=${GAME_TYPE}.love}
 WEB_DIR=public
 
 ./make-game.sh
-npx love.js ${GAME_ASSET} ${WEB_DIR} -t " "
+
+LoveJsVersion="0d78d942635570899ab81ffa9f00e70433a177af"
+LoveJsZip="${LoveJsVersion}.zip"
+LoveJsUrl="https://github.com/2dengine/love.js/archive/${LoveJsZip}"
+
+if [[ ! -e $LoveJsZip ]]
+then
+    curl -Lk -o $LoveJsZip $LoveJsUrl
+fi
+
+if [[ ! -e love.js-$LoveJsVersion ]]
+then
+    unzip $LoveJsZip -d .
+fi
+
+rm -rf $WEB_DIR
+mkdir -p $WEB_DIR/play/$LOVE_VERSION
+
+cp -r love.js-$LoveJsVersion/$LOVE_VERSION/release $WEB_DIR/play/$LOVE_VERSION/release
+cp love.js-$LoveJsVersion/*.{js,css} $WEB_DIR/play
+cp love.js-$LoveJsVersion/index.html $WEB_DIR/index.html 
+cp $GAME_ASSET $WEB_DIR/play/nogame.love
