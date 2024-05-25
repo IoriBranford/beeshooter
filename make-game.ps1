b35d3dae1fe4940e8f2cc26a5e3887c946e35692
IoriBranford <ioribranford@gmail.com>
2024-05-24 19:01:48 -0700

    Update make-game.ps1 to accept the path to the game files.

--- make-game.ps1
+++ make-game.ps1
@@ -1,7 +1,10 @@
 param(
 $GameType="game",
-$GameAsset="${GameType}.love"
+$GameAsset="${GameType}.love",
+$GamePath="game"
 )
 
 git describe --tags --always > version
-Compress-Archive -Path conf.lua,main.lua,libraries,code,shaders,version,data -DestinationPath $GameAsset -Force
+Set-Location $GamePath
+Compress-Archive -Path conf.lua,main.lua,libraries,code,shaders,version,data -DestinationPath "..\$GameAsset" -Force
+Set-Location ..
