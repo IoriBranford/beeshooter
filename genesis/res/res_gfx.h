6a19a2af748372fd7efff2be5c83df0806dc0de6
IoriBranford <ioribranford@gmail.com>
2024-06-05 16:20:18 -0700

    BG resource defs.

---
+++ genesis/res/res_gfx.h
@@ -0,0 +1,8 @@
+#ifndef _RES_RES_GFX_H_
+#define _RES_RES_GFX_H_
+
+extern const Palette bgPalette;
+extern const TileSet bgTileset;
+extern const MapDefinition bgMap;
+
+#endif // _RES_RES_GFX_H_
---
+++ genesis/res/res_gfx.res
@@ -0,0 +1,3 @@
+PALETTE bgPalette "bg.png"
+TILESET bgTileset "bg.png" BEST ALL
+MAP bgMap "bg.png" bgTileset BEST 0
