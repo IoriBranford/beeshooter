1c8faa270e604f588a37e38c858760bb677f0894
IoriBranford <ioribranford@gmail.com>
2024-06-05 16:20:47 -0700

    Try drawing and scrolling BG.

--- genesis/src/main.c
+++ genesis/src/main.c
@@ -5,12 +5,27 @@
  **/
 #include <genesis.h>
 
+#include "res_gfx.h"
+
 int main()
 {
-    VDP_drawText("Hello Sega!!", 10,13);
+    VDP_setScreenWidth256();
+    DMA_setBufferSize(8192);
+    DMA_setMaxTransferSize(8192);
+    
+    PAL_setPalette(PAL0, bgPalette.data, DMA);
+
+    VDP_loadTileSet(&bgTileset, TILE_USER_INDEX, DMA);
+    Map *bg = MAP_create(&bgMap, BG_A,
+        TILE_ATTR_FULL(0, FALSE, FALSE, FALSE, TILE_USER_INDEX));
+
+    VDP_setBackgroundColor(1);
+    
+    fix32 y = FIX32(4352);
     while(1)
     {
-        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        MAP_scrollTo(bg, 0, fix32ToInt(y));
+        y += -FIX32(3) / 4;
         SYS_doVBlankProcess();
     }
     return (0);
