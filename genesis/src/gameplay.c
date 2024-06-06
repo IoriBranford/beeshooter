3bf63a547d7ecb75116558a634efe82bfefd282d
IoriBranford <ioribranford@gmail.com>
2024-06-06 16:27:04 -0700

    Define a gameplay loop.

---
+++ genesis/src/gameplay.c
@@ -0,0 +1,68 @@
+#include <genesis.h>
+#include "gobject.h"
+#include "res_gfx.h"
+#include "res_audio.h"
+
+#include "player.h"
+
+static bool paused;
+static PlayerObject player;
+
+static void gameplay_joyEvent(u16 joy, u16 button, u16 state) {
+    if (joy == JOY_1) {
+        if (button == BUTTON_START && (state & button)) {
+            if (!paused) {
+                paused = true;
+                XGM2_pause();
+            } else {
+                paused = false;
+                XGM2_resume();
+            }
+        } else {
+            PLAYER_joyEvent(&player, button, state);
+        }
+    }
+}
+
+int gameplay() {
+    int result = 0;
+
+    DMA_setBufferSize(8192);
+    DMA_setMaxTransferSize(8192);
+
+    PAL_setPalette(PAL0, bgPalette.data, DMA);
+    PAL_setPalette(PAL1, palPlayer.data, DMA);
+
+    VDP_loadTileSet(&bgTileset, TILE_USER_INDEX, DMA);
+    Map *bg = MAP_create(&bgMap, BG_A,
+        TILE_ATTR_FULL(0, FALSE, FALSE, FALSE, TILE_USER_INDEX));
+
+    VDP_setBackgroundColor(1);
+
+    fix32 y = FIX32(4352);
+    MAP_scrollTo(bg, 0, fix32ToInt(y));
+    SYS_doVBlankProcess();
+
+    XGM2_play(bgm);
+
+    SPR_reset();
+
+    PLAYER_init(&player);
+    JOY_setEventHandler(gameplay_joyEvent);
+
+    while(1)
+    {
+        if (paused) {
+        } else {
+            player.update((Object*)&player);
+            y += -FIX32(3) / 4;
+            MAP_scrollTo(bg, 0, fix32ToRoundedInt(y));
+            SPR_update();
+        }
+        SYS_doVBlankProcess();
+    }
+
+    MAP_release(bg);
+
+    return result;
+}
