218daa6d77a9e750e0d60e10423ee06e491f5768
IoriBranford <ioribranford@gmail.com>
2024-06-06 16:31:39 -0700

    Use gameplay loop.

--- genesis/src/main.c
+++ genesis/src/main.c
@@ -8,37 +8,15 @@
 #include "res_gfx.h"
 #include "res_audio.h"
 
+int gameplay();
+
 int main()
 {
+    VDP_setScreenWidth256();
     SPR_init();
 
-    VDP_setScreenWidth256();
-    DMA_setBufferSize(8192);
-    DMA_setMaxTransferSize(8192);
-    
-    PAL_setPalette(PAL0, bgPalette.data, DMA);
-    PAL_setPalette(PAL1, palPlayer.data, DMA);
+    while (1)
+        gameplay();
 
-    VDP_loadTileSet(&bgTileset, TILE_USER_INDEX, DMA);
-    Map *bg = MAP_create(&bgMap, BG_A,
-        TILE_ATTR_FULL(0, FALSE, FALSE, FALSE, TILE_USER_INDEX));
-
-    VDP_setBackgroundColor(1);
-
-    Sprite* player = SPR_addSprite(&sprPlayer, 128-16, 112-16, TILE_ATTR(PAL1, TRUE, FALSE, FALSE));
-
-    fix32 y = FIX32(4352);
-    MAP_scrollTo(bg, 0, fix32ToInt(y));
-    SYS_doVBlankProcess();
-
-    XGM2_play(bgm);
-
-    while(1)
-    {
-        y += -FIX32(3) / 4;
-        MAP_scrollTo(bg, 0, fix32ToInt(y));
-        SPR_update();
-        SYS_doVBlankProcess();
-    }
     return (0);
 }
