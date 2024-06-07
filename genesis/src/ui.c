99b2798c5b5ca0fe51f18bfe3901c9e60832f792
IoriBranford <ioribranford@gmail.com>
2024-06-07 03:41:38 -0700

    HUD pulls score, timeleft, lives from player.

--- genesis/src/ui.c
+++ genesis/src/ui.c
@@ -4,18 +4,18 @@
 void UI_drawHud(PlayerObject *player) {
     char string[16];
 
-    int score = 0;
-    sprintf(string, "%08d", score);
+    u32 score = player->score;
+    sprintf(string, "%08lu", score);
     VDP_drawText(string, 7, 1);
 
-    int time = 60*60;
-    int frames = time % 60;
-    int seconds = (time / 60) % 60;
-    int minutes = min(time / 60 / 60, 99);
-    sprintf(string, "%02d:%02d:%02d", minutes, seconds, frames);
+    u16 time = player->timeLeft;
+    u8 frames = time % 60;
+    u8 seconds = (time / 60) % 60;
+    u8 minutes = min(time / 60 / 60, 99);
+    sprintf(string, "%02u:%02u:%02u", minutes, seconds, frames);
     VDP_drawText(string, 136>>3, 1);
 
-    int lives = 2;
+    u8 lives = player->lives;
     int numPrinted = 0;
     if (lives > 9)
         numPrinted = sprintf(string, "\x7e %d", lives);
@@ -24,7 +24,7 @@ void UI_drawHud(PlayerObject *player) {
     VDP_drawText(string, 1, 208>>3);
     VDP_clearText(numPrinted + 1, 208>>3, 9 - numPrinted);
 
-    int speed = player->speed > 2 ? 2 : 1;
+    u8 speed = player->speed > 2 ? 2 : 1;
     numPrinted = sprintf(string, "Speed %.*s", speed, "\x7f\x7f");
     VDP_drawText(string, 184>>3, 208>>3);
     VDP_clearText(numPrinted + (184>>3), 208>>3, 8 - numPrinted);
