dcb5f8267c89d31ac74f992c41cfde40f025180e
IoriBranford <ioribranford@gmail.com>
2024-06-06 23:02:08 -0700

    Fix HUD flicker.
    Only clear it where necessary.

--- genesis/src/ui.c
+++ genesis/src/ui.c
@@ -6,7 +6,6 @@ void UI_drawHud(PlayerObject *player) {
 
     int score = 0;
     sprintf(string, "%08d", score);
-    VDP_clearText(7, 1, 8);
     VDP_drawText(string, 7, 1);
 
     int time = 60*60;
@@ -14,19 +13,19 @@ void UI_drawHud(PlayerObject *player) {
     int seconds = (time / 60) % 60;
     int minutes = min(time / 60 / 60, 99);
     sprintf(string, "%02d:%02d:%02d", minutes, seconds, frames);
-    VDP_clearText(136>>3, 1, 8);
     VDP_drawText(string, 136>>3, 1);
 
     int lives = 2;
+    int numPrinted = 0;
     if (lives > 9)
-        sprintf(string, "{ %d", lives);
+        numPrinted = sprintf(string, "{ %d", lives);
     else
-        sprintf(string, "%.*s", lives, "{{{{{{{{{");
-    VDP_clearText(1, 208>>3, 9);
+        numPrinted = sprintf(string, "%.*s", lives, "{{{{{{{{{");
     VDP_drawText(string, 1, 208>>3);
+    VDP_clearText(numPrinted + 1, 208>>3, 9 - numPrinted);
 
     int speed = player->speed > 2 ? 2 : 1;
-    sprintf(string, "Speed %.*s", speed, "\x7f\x7f");
-    VDP_clearText(184>>3, 208>>3, 8);
+    numPrinted = sprintf(string, "Speed %.*s", speed, "\x7f\x7f");
     VDP_drawText(string, 184>>3, 208>>3);
+    VDP_clearText(numPrinted + (184>>3), 208>>3, 8 - numPrinted);
 }
