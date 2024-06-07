5f4a4e6b9944a66687a0ec6135d68462222615ab
IoriBranford <ioribranford@gmail.com>
2024-06-06 21:04:45 -0700

    Game object sprite util functions.

--- genesis/src/gobject.c
+++ genesis/src/gobject.c
@@ -1,10 +1,31 @@
 #include "gobject.h"
+#include "gameplay.h"
 
-void GOBJ_updateSprite(GameObject *this) {
-    Sprite *sprite = this->sprite;
+void GOBJ_init(GameObject *self) {
+    self->velX = self->velY = 0;
+}
+
+bool GOBJ_isRectOverlapping(GameObject *self, fix16 x, fix16 y, fix16 w, fix16 h) {
+    Sprite *sprite = self->sprite;
+    fix16 hw = FIX16(sprite->definition->w >> 1);
+    fix16 hh = FIX16(sprite->definition->h >> 1);
+    return (self->centerX + hw > x && self->centerX - hw < x + w
+        && self->centerY + hh > y && self->centerY - hh < y + h);
+}
+
+bool GOBJ_isSpriteOnScreen(GameObject *self) {
+    return GOBJ_isRectOverlapping(self, 0, 0, GAME_BOUNDW, GAME_BOUNDH);
+}
+
+void GOBJ_updateSprite(GameObject *self) {
+    Sprite *sprite = self->sprite;
     if (!sprite)
         return;
-    SPR_setPosition(this->sprite,
-        fix16ToRoundedInt(this->centerX) - (sprite->definition->w >> 1),
-        fix16ToRoundedInt(this->centerY) - (sprite->definition->h >> 1));
+    SPR_setPosition(self->sprite,
+        fix16ToRoundedInt(self->centerX) - (sprite->definition->w >> 1),
+        fix16ToRoundedInt(self->centerY) - (sprite->definition->h >> 1));
+}
+
+void GOBJ_release(GameObject *self) {
+    SPR_releaseSprite(self->sprite);
 }
--- genesis/src/gobject.h
+++ genesis/src/gobject.h
@@ -1,14 +1,22 @@
-#ifndef _OBJECT_H
-#define _OBJECT_H
+#ifndef _GOBJECT_H
+#define _GOBJECT_H
 
-#include <genesis.h>
+#include "types.h"
+#include "object.h"
+#include "sprite_eng.h"
 
 typedef struct {
     Object;
     Sprite *sprite;
     fix16 centerX, centerY;
+    fix16 velX, velY;
 } GameObject;
 
+bool GOBJ_isSpriteOnScreen(GameObject *self);
+bool GOBJ_isRectOverlapping(GameObject *self, fix16 x, fix16 y, fix16 w, fix16 h);
+
+void GOBJ_init(GameObject *self);
 void GOBJ_updateSprite(GameObject *this);
+void GOBJ_release(GameObject *self);
 
 #endif
