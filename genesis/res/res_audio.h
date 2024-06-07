5e6f4e7222c543536f64a7ace063159940d2f379
IoriBranford <ioribranford@gmail.com>
2024-06-07 01:39:46 -0700

    First sound assets.
    - Pause
    - Player shoot and change

--- genesis/res/res_audio.h
+++ genesis/res/res_audio.h
@@ -1,6 +1,11 @@
 #ifndef _RES_RES_AUDIO_H_
 #define _RES_RES_AUDIO_H_
 
+extern const u8 wavPause[3072];
+extern const u8 wavPlayerShot[5120];
+extern const u8 wavChangeWeapon[3840];
+extern const u8 wavChangeSpeedFast[5632];
+extern const u8 wavChangeSpeedSlow[5632];
 extern const u8 bgm[37120];
 
 #endif // _RES_RES_AUDIO_H_
--- genesis/res/res_audio.res
+++ genesis/res/res_audio.res
@@ -1,1 +1,7 @@
 XGM2 bgm funkbuster.vgm
+
+WAV wavPause "wav/pause.wav" XGM2
+WAV wavPlayerShot "wav/shootarrow.wav" XGM2
+WAV wavChangeWeapon "wav/changeweapon.wav" XGM2
+WAV wavChangeSpeedFast "wav/fast.wav" XGM2
+WAV wavChangeSpeedSlow "wav/slow.wav" XGM2
