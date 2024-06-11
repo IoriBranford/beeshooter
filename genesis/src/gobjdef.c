3bfa937f6e5f770b3a18bbdb7ad9fce4e670fdc0
IoriBranford <ioribranford@gmail.com>
2024-06-11 08:28:08 -0700

    Use GOBJ_updatePathWalker for Ant update. Add stubs for other GameObjectDefinitions.

--- genesis/src/gobjdef.c
+++ genesis/src/gobjdef.c
@@ -2,10 +2,28 @@
 #include "maths.h"
 #include "map.h"
 #include "res_gfx.h"
+#include "gobject.h"
 
 const GameObjectDefinition defAnt = {
     .health = 1, .speed = FIX16(2),
     .defeatPoints = 100,
     .spriteDef = &sprAnt,
-    .palette = &palAnt
+    .palette = &palAnt,
+    .update = GOBJ_updatePathWalker
 };
+const GameObjectDefinition defHoneyPot;
+const GameObjectDefinition defFly;
+const GameObjectDefinition defHoneyCell;
+const GameObjectDefinition defAntBig;
+const GameObjectDefinition defAlienGunner;
+const GameObjectDefinition defAlienPillager;
+const GameObjectDefinition defAlienMind;
+const GameObjectDefinition defAcidAnt;
+const GameObjectDefinition defWasp;
+const GameObjectDefinition defReinforcedHoneyPot;
+const GameObjectDefinition defAntHole;
+const GameObjectDefinition defBeetle;
+const GameObjectDefinition defWaspShooter;
+const GameObjectDefinition defWaspBoss;
+const GameObjectDefinition defTick;
+const GameObjectDefinition defWaspEgg;
