0e344ff34a138b93e419be8ec1b93c5327ff33fc
IoriBranford <ioribranford@gmail.com>
2024-06-11 00:15:30 -0700

    Implement basic bg/fg/flip actions.

--- genesis/src/actions.c
+++ genesis/src/actions.c
@@ -1,30 +1,41 @@
 #include "level.h"
 #include "gobject.h"
 
+#include <genesis.h>
+
 void spawnCharacters(Trigger *trigger)
 {
 }
 void stopStageScroll(Trigger *trigger)
 {
 }
+
 void faceRight(GameObject *self, PathPoint *pathPoint)
 {
+    SPR_setHFlip(self->sprite, false);
 }
+
 void PathPoint_SetSpeed(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void climbUpToFloor(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void enterForeground(GameObject *self, PathPoint *pathPoint)
 {
+    SPR_setPriority(self->sprite, true);
 }
+
 void startWaspAttack(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void markDisappear(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void BeetleShoot(GameObject *self, PathPoint *pathPoint)
 {
 }
@@ -34,18 +45,25 @@ void PathPoint_Rotate(GameObject *self, PathPoint *pathPoint)
 void PathPoint_SlowSpeedAndShootBurstsAtTarget(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void enterBackground(GameObject *self, PathPoint *pathPoint)
 {
+    SPR_setPriority(self->sprite, false);
 }
+
 void faceLeft(GameObject *self, PathPoint *pathPoint)
 {
+    SPR_setHFlip(self->sprite, true);
 }
+
 void ChooseSweepPath_DamageFlashRed(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void PathPoint_ShootBurstsAtAngle(GameObject *self, PathPoint *pathPoint)
 {
 }
+
 void ChargeAndLayEggs_DamageFlashRed(GameObject *self, PathPoint *pathPoint)
 {
 }
