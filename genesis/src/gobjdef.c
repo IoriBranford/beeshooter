2cdf731f5de27ab1878985dc4eadf3a31145570b
IoriBranford <ioribranford@gmail.com>
2024-06-13 04:36:30 -0700

    Define teams and body sizes for all current object defs.

--- genesis/src/gobjdef.c
+++ genesis/src/gobjdef.c
@@ -5,51 +5,65 @@
 #include "gobject.h"
 
 const GameObjectDefinition defAnt = {
+    .team = TEAM_ENEMY,
     .health = 1, .speed = FIX16(2),
     .defeatPoints = 200,
     .spriteDef = &sprAnt,
     .palette = &palAnt,
+    .bodyW = FIX16(16), .bodyH = FIX16(12),
     .update = GOBJ_updatePathWalker
 };
 const GameObjectDefinition defHoneyPot = {
+    .team = TEAM_ENEMY,
     .health = 1,
     .spriteDef = &sprHoneyPot,
     .palette = &palFlyAndHoney,
+    .bodyW = FIX16(16), .bodyH = FIX16(16),
     .update = GOBJ_updateIdleOnStage
 };
 const GameObjectDefinition defPowerup = {
+    .team = TEAM_ENEMY,
     .health = 1,
     .spriteDef = &sprPowerup,
     .palette = &palPlayer,
+    .bodyW = FIX16(16), .bodyH = FIX16(16),
     .update = GOBJ_updateIdleOnStage
 };
 const GameObjectDefinition defFly = {
+    .team = TEAM_ENEMY,
     .health = 8, .speed = FIX16(2),
     .defeatPoints = 500,
     .spriteDef = &sprFly,
     .palette = &palFlyAndHoney,
+    .bodyW = FIX16(28), .bodyH = FIX16(28),
     .update = GOBJ_updatePathWalker
 };
 const GameObjectDefinition defHoneyCell;
 const GameObjectDefinition defAntBig = {
+    .team = TEAM_ENEMY,
     .health = 16, .speed = FIX16(1),
     .defeatPoints = 2500,
     .spriteDef = &sprAntBig,
     .palette = &palAnt,
+    .bodyW = FIX16(40), .bodyH = FIX16(20),
     .update = GOBJ_updateIdleOnStage
 };
 const GameObjectDefinition defAlienGunner = {
+    .team = TEAM_ENEMY,
     .health = 8, .speed = FIX16(2),
     .defeatPoints = 500,
     .spriteDef = &sprAlien,
     .palette = &palAlien,
+    .bodyW = FIX16(20), .bodyH = FIX16(16),
     .update = GOBJ_updatePathWalker
 };
 const GameObjectDefinition defAlienPillager = {
+    .team = TEAM_ENEMY,
     .health = 4, .speed = FIX16(2),
     .defeatPoints = 250,
     .spriteDef = &sprAlien,
     .palette = &palAlien,
+    .bodyW = FIX16(20), .bodyH = FIX16(16),
     .animInd = 2,
     .update = GOBJ_updatePathWalker
 };
