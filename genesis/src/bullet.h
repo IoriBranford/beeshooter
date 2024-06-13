c32eca760418926b7d5f19ced0f6a6e102b7c576
IoriBranford <ioribranford@gmail.com>
2024-06-13 04:44:07 -0700

    Unexpose lower-level bullet create functions.

--- genesis/src/bullet.h
+++ genesis/src/bullet.h
@@ -4,8 +4,6 @@
 #include "types.h"
 #include "gobject.h"
 
-GameObject* BULLET_createVXVY(fix16 centerX, fix16 centerY, fix16 velX, fix16 velY);
-GameObject* BULLET_createAS(fix16 centerX, fix16 centerY, u16 angle, u16 speed);
 GameObject* BULLET_createAngAndDef(fix16 centerX, fix16 centerY, u16 angle, const GameObjectDefinition *def);
 
 void BULLET_update(GameObject *self);
