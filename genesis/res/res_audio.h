e1a68bd7ed12a522d1320d8b2bd516f0d780acf0
IoriBranford <ioribranford@gmail.com>
2024-06-13 20:08:54 -0700

    Add hit and defeat sound resources.

--- genesis/res/res_audio.h
+++ genesis/res/res_audio.h
@@ -6,6 +6,11 @@ extern const u8 wavPlayerShot[5120];
 extern const u8 wavChangeWeapon[3840];
 extern const u8 wavChangeSpeedFast[5632];
 extern const u8 wavChangeSpeedSlow[5632];
+extern const u8 wavPlayerShotHit[12288];
+extern const u8 wavBugKill1[4096];
+extern const u8 wavBugKill2[14336];
+extern const u8 wavBreakPot[8960];
+extern const u8 wavPowerup[12032];
 extern const u8 bgm[37120];
 
 #endif // _RES_RES_AUDIO_H_
--- genesis/res/res_audio.res
+++ genesis/res/res_audio.res
@@ -5,3 +5,9 @@ WAV wavPlayerShot "wav/shootarrow.wav" XGM2
 WAV wavChangeWeapon "wav/changeweapon.wav" XGM2
 WAV wavChangeSpeedFast "wav/fast.wav" XGM2
 WAV wavChangeSpeedSlow "wav/slow.wav" XGM2
+WAV wavPlayerShotHit "wav/arrowhit.wav" XGM2
+
+WAV wavBugKill1 "wav/bugkill1.wav" XGM2
+WAV wavBugKill2 "wav/bugkill2.wav" XGM2
+WAV wavBreakPot "wav/breakpot.wav" XGM2
+WAV wavPowerup "wav/powerup.wav" XGM2
