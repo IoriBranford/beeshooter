a475cc54e21b75ee6e1c96cac2f041c80cf4bdcb
IoriBranford <ioribranford@gmail.com>
2024-06-18 04:56:20 -0700

    Avoid accessing off the end of the tan table.

--- genesis/src/moremath.c
+++ genesis/src/moremath.c
@@ -4,10 +4,10 @@ extern const fix16 tantab_f16[256];
 
 fix16 tanFix16(u16 x) {
     if (x >= 768)
-        x = 1024 - (x % 1024);
+        x = 1023 - (x % 1024);
     else if (x >= 512)
         x = x - 512;
     else if (x >= 256)
-        x = 512 - x;
+        x = 511 - x;
     return tantab_f16[x];
 }
