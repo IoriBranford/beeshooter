64c16b624dc7285a7f38af4034c1beef4dcf398d
IoriBranford <ioribranford@gmail.com>
2024-07-09 13:51:35 -0700

    PAL_updateAlternatingColors

--- genesis/src/palette.c
+++ genesis/src/palette.c
@@ -1,19 +1,21 @@
 #include <genesis.h>
 #include "res_gfx.h"
 
-void PAL_updateFlyAndHoney(u16 paletteSlot) {
-    u16 color1 = palFlyAndHoney.data[1];
-    u16 color2 = palFlyAndHoney.data[2];
+void PAL_updateAlternatingColors(u16 paletteSlot, const Palette *palette, u8 i, u8 j) {
+    u16 color1 = palette->data[i];
+    u16 color2 = palette->data[j];
     if (getTick() % 30 >= 15) {
-        color1 = palFlyAndHoney.data[2];
-        color2 = palFlyAndHoney.data[1];
+        color1 = palette->data[j];
+        color2 = palette->data[i];
     }
-    PAL_setColor(paletteSlot*16 + 1, color2);
-    PAL_setColor(paletteSlot*16 + 2, color1);
+    PAL_setColor(paletteSlot*16 + i, color2);
+    PAL_setColor(paletteSlot*16 + j, color1);
 }
 
 void PAL_update(u16 paletteSlot, const Palette *palette) {
     if (palette == &palFlyAndHoney) {
-        PAL_updateFlyAndHoney(paletteSlot);
+        PAL_updateAlternatingColors(paletteSlot, palette, 1, 2);
+    } else if (palette == &palAcid) {
+        PAL_updateAlternatingColors(paletteSlot, palette, 6, 7);
     }
 }
