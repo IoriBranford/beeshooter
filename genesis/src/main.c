4b5b104875f164dab3f2e5b5f997e5ebca73ef9c
IoriBranford <ioribranford@gmail.com>
2024-07-18 16:42:53 -0700

    Init userdata on launch.

--- genesis/src/main.c
+++ genesis/src/main.c
@@ -3,6 +3,7 @@
  * Created With Genesis-Code extension for Visual Studio Code
  * Use "Genesis Code: Compile" command to compile this program.
  **/
+#include "userdata.h"
 #include <genesis.h>
 #include "res_gfx.h"
 #include "sounddef.h"
@@ -18,6 +19,8 @@ int main()
     VDP_loadFont(&font, DMA);
     VDP_setTextPriority(1);
 
+    USERDATA_init();
+    
     while (1) {
         title();
         SYS_reset();
