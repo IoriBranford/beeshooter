abf159c59b37d1c754a0e9c5cc352035f15cee09
IoriBranford <ioribranford@gmail.com>
2024-07-18 17:17:59 -0700

    Player zooms onto screen in title.

--- genesis/src/title.c
+++ genesis/src/title.c
@@ -20,17 +20,23 @@ int title() {
     PAL_setPalette(PAL_PLAYER_AND_BG, palPlayerAndBG.data, DMA);
     VDP_setTextPalette(PAL_PLAYER_AND_BG);
     VDP_drawImageEx(BG_B, &imgTitle, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, TILE_USER_INDEX), 0, 0, false, DMA);
+    s16 imageX = 256;
+    VDP_setHorizontalScroll(BG_B, imageX);
 
     DMA_waitCompletion();
     DMA_setBufferSizeToDefault();
     DMA_setMaxTransferSizeToDefault();
 
-    PAL_fadeInPalette(PAL0, imgTitle.palette->data, 10, false);
-
-    MENU_show(&MAIN_MENU);
-    JOY_setEventHandler(MENU_joyEvent);
+    PAL_fadeInPalette(PAL0, imgTitle.palette->data, 15, true);
 
     while (waitingToStart) {
+        if (imageX) {
+            VDP_setHorizontalScroll(BG_B, imageX -= 16);
+            if (!imageX) {
+                MENU_show(&MAIN_MENU);
+                JOY_setEventHandler(MENU_joyEvent);
+            }
+        }
         SYS_doVBlankProcess();
     }
 
@@ -39,5 +45,5 @@ int title() {
     VDP_clearTextArea(0, 0, 32, 28);
     VDP_clearPlane(BG_B, true);
 
-    GameResult result = gameplay();
+    return gameplay();
 }
