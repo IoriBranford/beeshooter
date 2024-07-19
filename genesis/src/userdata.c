00cca652257e6f53cb4de7c03828733586fdc92b
IoriBranford <ioribranford@gmail.com>
2024-07-19 16:31:02 -0700

    Simplify score offset calculation.

--- genesis/src/userdata.c
+++ genesis/src/userdata.c
@@ -23,15 +23,17 @@ static void saveName(UserData *data, u32 name) {
     SRAM_writeLong(fieldOffset(data, &data->name), name);
 }
 
-static void writeScore(u32 scoreOffset, HighScore *score) {
-    SRAM_writeLong(scoreOffset + fieldOffset(score, &score->name), score->name);
-    SRAM_writeLong(scoreOffset + fieldOffset(score, &score->bcdPoints), score->bcdPoints);
+static void writeScore(UserData *data, u8 i) {
+    HighScore *score = &data->highScores[i];
+    SRAM_writeLong(fieldOffset(data, &score->name), score->name);
+    SRAM_writeLong(fieldOffset(data, &score->bcdPoints), score->bcdPoints);
 }
 
-static void clearScore(u32 scoreOffset, HighScore *score) {
+static void clearScore(UserData *data, u8 i) {
+    HighScore *score = &data->highScores[i];
     score->name = ('N'<<24) + ('O'<<16) + ('N'<<8) + 'E';
     score->bcdPoints = 0;
-    writeScore(scoreOffset, score);
+    writeScore(data, i);
 }
 
 static void initData(UserData *data) {
@@ -52,18 +54,16 @@ static void initData(UserData *data) {
     }
 
     HighScore *scores = data->highScores;
-    u32 scoreOffset = fieldOffset(data, &data->highScores);
     for (int i = 0; i < NUM_SCORES; ++i) {
         HighScore *score = &scores[i];
-        u32 name = SRAM_readLong(scoreOffset + fieldOffset(score, &score->name));
+        u32 name = SRAM_readLong(fieldOffset(data, &score->name));
         if (name) {
-            u32 bcdPoints = SRAM_readLong(scoreOffset + fieldOffset(score, &score->bcdPoints));
+            u32 bcdPoints = SRAM_readLong(fieldOffset(data, &score->bcdPoints));
             score->name = name;
             score->bcdPoints = bcdPoints;
         } else {
-            clearScore(scoreOffset, score);
+            clearScore(data, i);
         }
-        scoreOffset += sizeof(HighScore);
     }
 }
 
@@ -72,7 +72,7 @@ static u8 addScore(UserData *data, u32 name, u32 bcdPoints) {
         return 0;
 
     HighScore *scores = data->highScores;
-    int i = 0;
+    u8 i = 0;
     for (i = 0; i < NUM_SCORES; ++i) {
         if (bcdPoints >= scores[i].bcdPoints)
             break;
@@ -80,17 +80,15 @@ static u8 addScore(UserData *data, u32 name, u32 bcdPoints) {
     if (i >= NUM_SCORES)
         return 0;
     
-    u32 scoreOffset = fieldOffset(data, &data->highScores) + (NUM_SCORES - 1) * sizeof(HighScore);
-    for (int j = NUM_SCORES - 1; j > i; --j) {
+    for (u8 j = NUM_SCORES - 1; j > i; --j) {
         scores[j] = scores[j-1];
-        writeScore(scoreOffset, &scores[j]);
-        scoreOffset -= sizeof(HighScore);
+        writeScore(data, j);
     }
 
     HighScore *score = &scores[i];
     score->name = name;
     score->bcdPoints = bcdPoints;
-    writeScore(scoreOffset, score);
+    writeScore(data, i);
 
     return i + 1;
 }
@@ -115,12 +113,8 @@ const HighScore* USERDATA_getScore(u32 i) {
 
 void USERDATA_clearScores() {
     UserData *data = &userData;
-    HighScore *scores = data->highScores;
-    u32 scoreOffset = fieldOffset(data, scores);
     for (int i = 0; i < NUM_SCORES; ++i) {
-        HighScore *score = &scores[i];
-        clearScore(scoreOffset, score);
-        scoreOffset += sizeof(HighScore);
+        clearScore(data, i);
     }
 }
 
