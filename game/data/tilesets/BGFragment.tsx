e763625d8fca439e87048b29d834c26a29046493
IoriBranford <ioribranford@gmail.com>
2024-07-22 22:17:20 -0700

    Give BGFragment tile a center pivot.

--- game/data/tilesets/BGFragment.tsx
+++ game/data/tilesets/BGFragment.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="BGFragment" tilewidth="16" tileheight="24" tilecount="4" columns="4">
+<tileset version="1.11" tiledversion="1.11.0" name="BGFragment" tilewidth="16" tileheight="24" tilecount="4" columns="4" objectalignment="center">
  <image source="BGFragment.png" width="64" height="24"/>
  <tile id="0">
   <animation>
