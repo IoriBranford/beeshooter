85fe3077ede98d8bd4d8807ff21caf9ccfce6e70
IoriBranford <ioribranford@gmail.com>
2024-07-22 22:06:53 -0700

    Background fragment for placing in map.

---
+++ game/data/tilesets/BGFragment.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="BGFragment" tilewidth="16" tileheight="24" tilecount="4" columns="4">
+ <image source="BGFragment.png" width="64" height="24"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
