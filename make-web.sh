7baa7fd4f5efa34e7888235738ae127f5bb71e23
IoriBranford <ioribranford@gmail.com>
2025-04-14 16:19:26 -0700

    love.js ver 1a6e4c7553b264a8205d63e8b598b8e6798265cb

--- make-web.sh
+++ make-web.sh
@@ -8,16 +8,16 @@ WEB_DIR=public
 
 ./make-game.sh
 
-LoveJsVersion="0d78d942635570899ab81ffa9f00e70433a177af"
+LoveJsVersion="1a6e4c7553b264a8205d63e8b598b8e6798265cb"
 LoveJsZip="${LoveJsVersion}.zip"
 LoveJsUrl="https://github.com/2dengine/love.js/archive/${LoveJsZip}"
 
-if [[ ! -e $LoveJsZip ]]
+if [ ! -e $LoveJsZip ]
 then
     curl -Lk -o $LoveJsZip $LoveJsUrl
 fi
 
-if [[ ! -e love.js-$LoveJsVersion ]]
+if [ ! -e love.js-$LoveJsVersion ]
 then
     unzip $LoveJsZip -d .
 fi
@@ -26,6 +26,6 @@ rm -rf $WEB_DIR
 mkdir -p $WEB_DIR/$LOVE_VERSION
 
 cp -r love.js-$LoveJsVersion/$LOVE_VERSION/release $WEB_DIR/$LOVE_VERSION/release
-cp love.js-$LoveJsVersion/*.js love.js-$LoveJsVersion/*.css $WEB_DIR
+cp love.js-$LoveJsVersion/*.js love.js-$LoveJsVersion/*.css love.js-$LoveJsVersion/fetch.lua $WEB_DIR
 cp love-js/index.html $WEB_DIR/index.html
 cp $GAME_ASSET $WEB_DIR/nogame.love
