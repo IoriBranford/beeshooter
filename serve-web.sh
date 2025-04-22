063149d74e3ceada9624e42dd139a17635b5b42d
IoriBranford <ioribranford@gmail.com>
2025-04-21 18:13:42 -0700

    serve-web.sh --https

--- serve-web.sh
+++ serve-web.sh
@@ -2,4 +2,4 @@
 
 WEB_DIR=${WEB_DIR:=public}
 
-npx ws --directory $WEB_DIR --cors.opener-policy same-origin --cors.embedder-policy require-corp
+npx ws --directory $WEB_DIR --https --cors.opener-policy same-origin --cors.embedder-policy require-corp
