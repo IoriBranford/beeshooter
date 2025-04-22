e17b9e17baa59d94c156b7c3655bd4a2eb10232a
IoriBranford <ioribranford@gmail.com>
2025-04-21 18:18:53 -0700

    Update copyright year in make-vars.sh

--- make-vars.sh
+++ make-vars.sh
@@ -4,6 +4,6 @@ export PROJECT=honey-guardian
 export GAME_TITLE="Honey Guardian"
 export GAME_TITLE_NOSPACE="Honey_Guardian"
 export DESCRIPTION="Caravan shooting"
-export COPYRIGHT="© 2022-2024 IoriBranford"
+export COPYRIGHT="© 2022-2025 IoriBranford"
 export LOVE_VERSION="11.5"
 export GAME_DIR="$PWD/game"
